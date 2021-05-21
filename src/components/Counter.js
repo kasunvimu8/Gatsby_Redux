77b5aa0673217021075808304df46d19875cb44a
v_kasun <v.kasun@Directfn.com>
2021-05-21 10:07:13 +0530

    styled counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,31 +1,52 @@
 import React  from 'react';
+import styled from "styled-components";
 import reduxStore from '../ReduxStore';
 import { useSelector } from 'react-redux';
 import {increment, decrement, reset} from '../Actions/CounterAction';
 
 const Counter = () => {
+    const Row = styled.div`
+        display: flex;
+        padding-top: 1em;
+    `;
+    const Counter = styled.h1`
+      justify-content: flex-start;
+      font-size:7em;
+    `;
+    const ButtonDiv = styled.div`
+      justify-content: flex-end;
+      padding-left: 2em;
+      font-size: 1em;
+    `;
+    const Btn = styled.button`
+      font-size: 1em;
+      margin: 0.25em;
+      padding: 0.25em 1em;
+      background-color: #97e597;
+      float:left; 
+      clear: left;
+    `;
+
     const counterData = useSelector(state => state.counterData);
 
     return (
-        <div>
-            <div><h2>{counterData}</h2></div>
-            <div>
-                <button onClick={() =>
+        <Row>
+            <Counter><div>{counterData}</div></Counter>
+            <ButtonDiv>
+                <Btn onClick={() =>
                     reduxStore.dispatch(increment(counterData))}>
                     INCREMENT
-                </button>
-            </div>
-            <div>
-                <button onClick={() =>
+                </Btn>
+                <Btn onClick={() =>
                     reduxStore.dispatch(decrement(counterData))}>
                     DECREMENT
-                </button>
-            </div>
-            <button onClick={() =>
-                reduxStore.dispatch(reset())}>
-                RESET
-            </button>
-        </div>
+                </Btn>
+                <Btn onClick={() =>
+                    reduxStore.dispatch(reset())}>
+                    RESET
+                </Btn>
+            </ButtonDiv>
+        </Row>
     );
 }
 
